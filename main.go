ed0a799b15348e1b27bc97f0893ac69fefa96d10
Umayr Shahid <umayrr@hotmail.co.uk>
2017-08-09 15:53:50 +0500

    Add source

---
+++ main.go
@@ -0,0 +1,18 @@
+// #cgo pkg-config: openssl
+package main
+
+import (
+	"openssl"
+	"fmt"
+)
+
+
+func main()  {
+	HelloWorld()
+}
+
+func HelloWorld()  {
+	status, version := openssl.Status()
+	fmt.Println("status", status)
+	fmt.Println("version",version)
+}
